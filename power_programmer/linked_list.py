ebda7cb3b3f4f215ef0e2657f9cada40b9c83e62
Utsav Raut <utsav.raut@yahoo.com>
2019-12-05 08:01:44 +0530

    Added delete functionality to linked list file

--- power_programmer/linked_list.py
+++ power_programmer/linked_list.py
@@ -7,6 +7,12 @@ class LinkedList:
     def __init__(self):
         self.head = None
 
+    def print_list(self):
+        cur_node = self.head
+        while cur_node: #means current node is not null
+            print(cur_node.data)
+            cur_node = cur_node.next
+
     def append(self, data):
         new_node = Node(data)
         if self.head is None:
@@ -31,22 +37,56 @@ class LinkedList:
         new_node.next = prev_node.next
         prev_node.next = new_node
 
-    def print_list(self):
+    def delete_node(self, key):
         cur_node = self.head
-        while cur_node: #means current node is not null
-            print(cur_node.data)
+        if cur_node and cur_node.data == key:
+            self.head = cur_node.next
+            cur_node =  None
+            return
+
+        prev = None
+        while cur_node and cur_node.data != key:
+            prev = cur_node
             cur_node = cur_node.next
+
+        if cur_node is None: #Element not present
+            return
+
+        prev.next = cur_node.next
+        cur_node = None
+
+    def delete_node_at_pos(self, pos):
+        cur_node = self.head
+        if pos == 0:
+            self.head = cur_node.next
+            cur_node = None
+            return
+        
+        prev = None
+        count = 1
+        while cur_node and count != pos:
+            prev = cur_node
+            cur_node = cur_node.next
+            count += 1
+        
+        if cur_node is None:
+            return
+        
+        prev.next = cur_node.next
+        cur_node = None
 
 llist = LinkedList()
 llist.append("A")
 llist.append("B")
 llist.append("C")
-llist.append("D")
-llist.print_list()
+# llist.append("D")
+# llist.print_list()
 print("\n")
 # llist.prepend("E")
 # llist.print_list()
 # print(llist.head.data)
 # print(llist.head.next.data)
-llist.insert_after_node(llist.head.next, "E")
+# llist.insert_after_node(llist.head.next, "E")
+# llist.delete_node("B")
+llist.delete_node_at_pos(2)
 llist.print_list()
